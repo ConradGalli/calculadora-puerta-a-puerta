2b5e52987297f8bd841193b83c92f5e50588a7f8
lucasromerodb <lucasromerodb@gmail.com>
2017-09-15 00:28:29 -0300

    diseño + redes sociales + meta img + boton para volver arriba

--- src/frontend/js/scripts/2-front.js
+++ src/frontend/js/scripts/2-front.js
@@ -139,3 +139,30 @@ function f_keypress__enter(e) {
 			f_book_condition();
 	}
 }
+
+function f_social__share() {
+	// No usar "|" (pipe) o caracteres especiales en el <title> del html
+	var	myUrl = window.location.href,
+			title = $(document).find("title").text(),
+			facebook = 'https://www.facebook.com/sharer/sharer.php?u='+myUrl+'',
+			twitter = 'https://twitter.com/intent/tweet?text='+title+'%20'+myUrl+'';
+
+	// Compartir en Facebook
+	$('.c-navigation__link_facebook').on('click', function(event) {
+		console.log('Url a compartir: '+myUrl);
+		event.preventDefault();
+		window.open(facebook, 'Compartir en Facebook', 'width=500,height=500,resizable=1');
+	});
+
+	// Compartir en Twitter
+	$('.c-navigation__link_twitter').on('click', function(event) {
+		console.log('Url a compartir: '+myUrl);
+		event.preventDefault();
+		window.open(twitter, 'Compartir en Twitter', 'width=500,height=500,resizable=1');
+	});
+
+	// Compartir en Whatsapp
+	// Este atributo abre la app de whatsapp si la tenemos instalada (lo que es muy probable)
+	$('.c-navigation__link_whatsapp')
+		.attr('href', 'whatsapp://send?text='+title+'%20' + myUrl + '');
+}
